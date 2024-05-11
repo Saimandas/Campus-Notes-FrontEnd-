4b813cf954d2b303cb75a5135ff05eb52a0155e5
Saimandas <dhirajdas55555@gmail.com>
2024-05-11 13:35:17 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-
+const port = import.meta.env.VITE_PORT
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
       "/users":"http://localhost:3000/api/v1"
     },
-    port:3000
+    port:port
   },
   plugins: [react()],
   resolve: {
