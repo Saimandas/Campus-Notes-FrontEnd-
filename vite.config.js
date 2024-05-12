9e82d71cab4ae0df1ff6af5d629ff2e82965f628
Saimandas <dhirajdas55555@gmail.com>
2024-05-12 13:36:13 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // Define the port directly or use a default value
-const port = process.env.PORT 
+const port = import.meta.env.VITE_PORT || 3000
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
-      "/users":"http://localhost:3000/api/v1"
+      "/users":"https://campusnotes-backend-1.onrender.com/api/v1"
     },
     port: port
   },
