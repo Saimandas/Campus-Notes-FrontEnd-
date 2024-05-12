eb40100109788b08afa8f0a0f2e2bc297e20f377
Saimandas <dhirajdas55555@gmail.com>
2024-05-12 13:28:55 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // Define the port directly or use a default value
-const port = import.meta.env.VITE_PORT || 4000;
+const port = process.env.PORT 
 
 // https://vitejs.dev/config/
 export default defineConfig({
