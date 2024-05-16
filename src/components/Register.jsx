63ce6a0b46654aa0c5fbe3f0d3c4b3c69a004054
Saimandas <dhirajdas55555@gmail.com>
2024-05-16 21:29:10 +0530

    google auth

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -15,6 +15,16 @@ const Register = () => {
    const debounce= useDebouncedCallback((e)=>{setusername(e) },500)
    const [signUpMessage, setsignUpMessage] = useState("")
    const navigate= useNavigate()
+   const google= async ()=>{
+    try {
+      const response= await axios.get("users/loginWithGoogle")
+      setsignUpMessage(response.data.message)
+      console.log(response);
+    } catch (error) {
+      setsignUpMessage(error.response.data.message)
+      console.log(error);
+    }
+   }
    useEffect(()=>{
     async function check(){
       if (username) {
@@ -65,25 +75,31 @@ const Register = () => {
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
               <div>
-                      <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
                       <input type="username" value={displayUsername} onChange={(e)=>{debounce(e.target.value),setdisplayUsername(e.target.value)}}   name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required=""/>
                       { isCheckingUsername&&username!=="" && <p className={userameMessage==="username is unique"? ' text-green-500':' text-red-500'}>{userameMessage}</p>}
                   </div>
                   <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" value={email} onChange={(e)=>{setemail(e.target.value)}} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required=""/>
                   </div>
                   <div>
-                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                       <input type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
+                  <div className="flex items-center justify-center w-full  dark:bg-gray-800">
+    <button className="px-4 py-2 border  gap-2 w-full flex justify-center border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" onClick={google}>
+        <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+        <span>Continue with Google</span>
+    </button>
+</div>
                   <div className="flex items-center justify-between">
                       <div className="flex items-start">
                           <div className="flex items-center h-5">
                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                           </div>
                           <div className="ml-3 text-sm">
-                            <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                            <label className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
                       
@@ -96,8 +112,8 @@ const Register = () => {
                     signUpMessage && <p className="text-gray-500 dark:text-gray-300">{signUpMessage}</p>
                   }
                   
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      already have an account yet?<Link to={'/login'}><h1 href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</h1></Link>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      already have an account yet?<Link to={'/login'}><h1 href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</h1></Link>
                   </p>
               </form>
           </div>
