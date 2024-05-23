3ba4c675f1297b6dbcdb37d51bb13a66e2348f02
Saimandas <dhirajdas55555@gmail.com>
2024-05-23 21:14:25 +0530

    first commit

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -46,18 +46,18 @@ const Upload = () => {
                 <label><h1 className=" text-black font-semibold text-lg">Notes Name:</h1></label>
                 <input type="text" value={NotesName} required={true} onChange={(e)=>{setNotesName(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-2"/> 
 
-                <label><h1 className=" text-black font-semibold text-lg mt-4">Subject:</h1></label>
-                <select className=" mt-1 w-40 mt-2 rounded-md p-2 h-12 bg-blue-700 text-white font-semibold outline-blue-800"  value={subject} required={true} onChange={(e)=>{setsubject(e.target.value)}}>
-                    <option value="DSA" className=" bg-gray-700 hover:bg-gray-600 ">DSA</option>
-                    <option value="Maths" className=" bg-gray-700 hover:bg-gray-600 ">Maths</option>
-                    <option value="Fundamentels" className=" bg-gray-700 hover:bg-gray-600 ">Fundamentels</option>
-                </select>
-
                 <label><h1 className=" text-black font-semibold text-lg mt-4">Depertment:</h1></label>
                 <select className=" w-40 mt-2 rounded-md p-2 h-12 bg-red-700 text-white font-semibold outline-red-500" value={depertment} required={true} onChange={(e)=>{setdepertment(e.target.value)}}>
                     <option value="BCA" className=" bg-gray-700 hover:bg-gray-600">BCA</option>
                     <option value="BA" className=" bg-gray-700 hover:bg-gray-600">BA</option>
                     <option value="BSC" className=" bg-gray-700 hover:bg-gray-600">BSC</option>
+                </select>
+
+                <label><h1 className=" text-black font-semibold text-lg mt-4">Subject:</h1></label>
+                <select className=" mt-1 w-40 mt-2 rounded-md p-2 h-12 bg-blue-700 text-white font-semibold outline-blue-800"  value={subject} required={true} onChange={(e)=>{setsubject(e.target.value)}}>
+                    <option value="DSA" className=" bg-gray-700 hover:bg-gray-600 ">DSA</option>
+                    <option value="Maths" className=" bg-gray-700 hover:bg-gray-600 ">Maths</option>
+                    <option value="Fundamentels" className=" bg-gray-700 hover:bg-gray-600 ">Fundamentels</option>
                 </select>
                 <label> <h1 className="text-black font-semibold text-lg mt-4">Select Image</h1></label>
                 <input type="file" name="notesImg" accept=".png,.jpg" capture required={true}  onChange={(e)=>{setNotesImg(e.target.files[0])}} />
